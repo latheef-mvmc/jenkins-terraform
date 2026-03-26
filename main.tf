bc7d4991d5d333fa1352818340e97fc7f98a3b89
latheefmvmc <latheef.mvmc@gmail.com>
2026-03-26 06:12:45 +0000

    subnet added

--- main.tf
+++ main.tf
@@ -11,4 +11,10 @@ resource "aws_vpc" "demo-vpc" {
   
 }
 
-
+resource "aws_subnet" "demo-subnet" {
+    vpc_id = aws_vpc.demo-vpc.id
+    cidr_block = "90.0.1.0/24"
+    tags = {
+        Name = "demo-subnet"
+    }
+}
