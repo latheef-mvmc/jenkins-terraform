aa47d70e5633bab1024bf65031b8bd5c2ce192f5
latheefmvmc <latheef.mvmc@gmail.com>
2026-03-27 04:28:44 +0000

    bbb

--- main.tf
+++ main.tf
@@ -6,14 +6,14 @@ provider "aws" {
 resource "aws_vpc" "demo-vpc" {
     cidr_block = "90.0.0.0/16"
     tags = {
-        Name = "IBM-VPC"
+        Name = "IBM-VPC-Demo"
     }
   
 }
 
 resource "aws_subnet" "demo-subnet" {
     vpc_id = aws_vpc.demo-vpc.id
-    cidr_block = "90.0.2.0/24"
+    cidr_block = "90.0.3.0/24"
     tags = {
         Name = "IBM-VPC-public-subnet1"
     }
