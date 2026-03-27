279c2475a902716596e838afb896d38cc4f1eb83
latheef-mvmc <107998191+latheef-mvmc@users.noreply.github.com>
2026-03-27 19:52:21 +0530

    Create main.tf
    new code

---
+++ main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+variable "env" {
+  description = "Environment"
+  type        = string
+}
+
+resource "aws_vpc" "demo-vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "demo-vpc-${var.env}"
+  }
+}
+
+resource "aws_subnet" "demo-subnet" {
+  vpc_id     = aws_vpc.demo-vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "demo-subnet-${var.env}"
+  }
+}
